16c68f0af27dbbc3784fc7cb431cc6c00221dec9
atanas011 <atanas011@yahoo.com>
2023-01-24 20:51:35 +0100

    Basic Features

--- src/test/java/tests/PlaceNameTest.java
+++ src/test/java/tests/PlaceNameTest.java
@@ -1,19 +1,43 @@
 package tests;
 
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.*;
 
 public class PlaceNameTest {
 
+    private final String url = "http://zippopotam.us/us/90210";
+
     @Test
-    public void testPlaceName() {
-        given().
-        when().
-            get("http://zippopotam.us/us/90210").
-        then().
-            assertThat().
-            body("places[0].'place name'", equalTo("Beverly Hills"));
+    public void logReqResDetails() {
+        given().log().all().when().get(url).then().log().body();
+    }
+
+    @Test
+    public void checkContentType() {
+        assertResponse().
+            contentType(ContentType.JSON); // ("application/json");
+    }
+
+    @Test
+    public void checkStatusCode() {
+        assertResponse().statusCode(200);
+    }
+
+    @Test
+    public void checkResBody() {
+        String jsonPathState = "places[0].state";
+        String jsonPathPlace = "places[0].'place name'"; // 2 words, hence ''
+        assertResponse().body(jsonPathState, equalTo("California"));
+        assertResponse().body(jsonPathPlace, equalTo("Beverly Hills"));
+        assertResponse().body("places.'place name'", hasItem("Beverly Hills"));
+        assertResponse().body("places.'place name'", hasSize(1));
+    }
+
+    private ValidatableResponse assertResponse() {
+        return given().when().get(url).then().assertThat();
     }
 }
